533d9ac8c7a2ac8d719e5a0241f5620274a237fd
alinplotnik <alin.plotnikov@runi.ac.il>
2024-02-03 15:52:16 +0200

    try 2

--- SpellChecker.java
+++ SpellChecker.java
@@ -12,10 +12,29 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		// Your code goes here
+      return null;
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		word1.toLowerCase();
+		word2.toLowerCase();
+
+		if ( word2.isEmpty() )
+		{
+			return word1.length();
+		}
+         
+		if ( word1.isEmpty() )
+		{
+			return word2.length();
+		}
+         
+		if ( word1.charAt(0) == word2.charAt(0))
+		{
+			return levenshtein(word1.substring(1), word2.substring(1));
+		}
+
+		return 1 + Math.min(Math.min(levenshtein(word1.substring(1), word2), levenshtein(word1, word2.substring(1))), levenshtein(word1.substring(1), word2.substring(1)));
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -25,11 +44,12 @@ public class SpellChecker {
 
 		// Your code here
 
-		return dictionary;
+		return null;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		return null;
 	}
 
 }
